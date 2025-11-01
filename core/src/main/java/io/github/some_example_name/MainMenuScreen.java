ed5cf85a74261ed2c4c59806669aa9e62242ac34
p-knight <fares.s.f.almutairi@gmail.com>
2025-11-01 00:28:23 +0000

    added a menuscreen. a temp tutorial screen i took from the internet as a place holder(i will change it later) and an exit button.

--- core/src/main/java/io/github/some_example_name/MainMenuScreen.java
+++ core/src/main/java/io/github/some_example_name/MainMenuScreen.java
@@ -1,14 +1,24 @@
 package io.github.some_example_name;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainMenuScreen implements Screen {
     final StumbleHome game;
+    private Texture background;
+    private Texture tutorialImage;
+    private Stage stage;
+    private Skin skin;
+    private boolean showTutorial = false;
 
     public MainMenuScreen(final StumbleHome game) {
         this.game = game;
@@ -16,15 +26,57 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(new InputAdapter(){
+
+        background = new Texture("background.jpg");
+        tutorialImage = new Texture("tutorialImage.jpg");
+
+        stage = new Stage(new ScreenViewport());
+        skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+
+        // Added: create buttons
+        TextButton playButton = new TextButton("Play", skin);
+        TextButton tutorialButton = new TextButton("Tutorial", skin);
+        TextButton exitButton = new TextButton("Exit", skin);
+
+        // Added: set button positions
+        float centerX = Gdx.graphics.getWidth() / 2f - 100;
+        float startY = Gdx.graphics.getHeight() / 2f + 50;
+
+        playButton.setBounds(centerX, startY, 200, 50);
+        tutorialButton.setBounds(centerX, startY - 70, 200, 50);
+        exitButton.setBounds(centerX, startY - 140, 200, 50);
+
+        // Added: add buttons to stage
+        stage.addActor(playButton);
+        stage.addActor(tutorialButton);
+        stage.addActor(exitButton);
+
+        //  Added: handle Play button click
+        playButton.addListener(new ClickListener() {
             @Override
-            public boolean keyDown(final int keycode) {
-                if (keycode == Input.Keys.SPACE) {
-                    game.setScreen(new GameScreen(game));
-                }
-                return true;
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new GameScreen(game));
             }
         });
+
+        // Added: handle Tutorial button click
+        tutorialButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                showTutorial = true; // show popup when clicked
+            }
+        });
+
+        // Added: handle Exit button click
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        // Added: set stage to receive input
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
@@ -32,9 +84,34 @@ public class MainMenuScreen implements Screen {
         ScreenUtils.clear(Color.BLUE);
 
         game.batch.begin();
+        if (background != null) {
+            game.batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        }
+
         game.font.draw(game.batch, "Title Screen!", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight()* .75f);
-        game.font.draw(game.batch, "Press space to play.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight()* .5f);
+
+
         game.batch.end();
+
+        if (stage != null) {
+            stage.act(delta);
+            stage.draw();
+
+
+        }
+
+        // Added: draw tutorial popup if active
+        if (showTutorial && tutorialImage != null) {
+            game.batch.begin();
+            game.batch.draw(
+                tutorialImage,
+                Gdx.graphics.getWidth() / 2f - tutorialImage.getWidth() / 2f,
+                Gdx.graphics.getHeight() / 2f - tutorialImage.getHeight() / 2f
+            );
+            game.batch.end();
+        }
+
+
     }
 
     @Override
@@ -55,5 +132,9 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
+        if (background != null) background.dispose();
+        if (tutorialImage != null) tutorialImage.dispose();
+        if (stage != null) stage.dispose();
+        if (skin != null) skin.dispose();
     }
 }
