4e82f1e74574aa59024b0cdff8c59b04ec5103e7
Starman7312 <henrygillett@duck.com>
2025-12-04 15:09:06 +0000

    feat: gradle setup for headless testing and base test files added based off gemo example

---
+++ headless/src/test/java/io/github/stumblehome/headless/AssetTests.java
@@ -0,0 +1,15 @@
+package io.github.stumblehome.headless;
+
+import com.badlogic.gdx.Gdx;
+// Change for test
+// import uk.ac.york.cs.eng1.gemo.Ship;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AssetTests extends AbstractHeadlessGdxTest {
+    @Test
+    public void testShipAssetExists() {
+        System.out.println("T");;
+    }
+}
