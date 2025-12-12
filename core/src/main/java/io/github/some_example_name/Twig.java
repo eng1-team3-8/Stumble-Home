bd25fa9f88de768d39a4e0800e2a41c23f40831f
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-12-12 15:26:08 +0000

    feat: Created the twig class

---
+++ core/src/main/java/io/github/some_example_name/Twig.java
@@ -0,0 +1,13 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Twig extends CollidableEntity{
+
+    public Twig(Texture texture){
+        super(texture);
+    }
+
+}
