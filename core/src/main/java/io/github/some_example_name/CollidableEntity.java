74f2fc71b69a0f507df682784f198e789170248a
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-12-12 15:25:57 +0000

    feat: Created CollidableEntity class

---
+++ core/src/main/java/io/github/some_example_name/CollidableEntity.java
@@ -0,0 +1,13 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public abstract class CollidableEntity extends NonPlayerEntity{
+
+    public CollidableEntity(Texture texture){
+        super(texture);
+    }
+
+
+}
