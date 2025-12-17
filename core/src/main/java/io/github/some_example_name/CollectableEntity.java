c023125176d7bf7e858aaaeb4afaf913c42b41dc
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-12-17 15:04:56 +0000

    Feat: Added the collision checker to the class, and added the constructor

--- core/src/main/java/io/github/some_example_name/CollectableEntity.java
+++ core/src/main/java/io/github/some_example_name/CollectableEntity.java
@@ -2,7 +2,40 @@ package io.github.some_example_name;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class CollectableEntity {
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+public abstract class CollectableEntity extends NonPlayerEntity{
+
+    // Boolean value to see if the entity is colliding with the player
+    boolean isColliding = false;
+
+    // Boolean value to see if the entity has been collected
+    boolean isCollected = false;
+
+    public CollectableEntity(Texture texture, Sprite sprite, float size, int frame_width, int frame_height, int num_animations){
+        super(sprite, size, frame_width, frame_height, num_animations);
+    }
+
+    public void checkColliding(float playerX, float playerY){
+        // Checks if the entity has already been collected
+        if (this.isCollected){
+            return;
+        }
+
+        // Calculates the distance between the player and the entity using pythagoras
+        double distance = sqrt(pow(playerX - this.getCentreX(), 2) + pow(playerY - this.getCentreY(), 2));
+
+        // Sets the colliding variable to true if the player is within a certain distance
+        this.isCollected = distance < 1f;
+    }
+
+
+    @Override
+    public void logic() {
+
+    }
 
 }
