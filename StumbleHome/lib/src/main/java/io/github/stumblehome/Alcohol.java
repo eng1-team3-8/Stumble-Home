c25618deae4edac763cef0355b720073f0f4dce7
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-12-21 18:37:51 +0000

    Feat: Added the alcohol class, for any alcoholic beverages

---
+++ StumbleHome/lib/src/main/java/io/github/stumblehome/Alcohol.java
@@ -0,0 +1,23 @@
+package io.github.stumblehome;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Alcohol extends CollectableEntity {
+    /**
+     * A constructor for entities that don't have any animations
+     * @param texture Texture: The texture of the entity
+     * @param size float: The size of the entity
+     * @param position float[X,Y]: A float array of the coordinates of the entity. Position 0 is X, and 1 is Y.
+     */
+    public Alcohol(Texture texture, float size, float[] position) {
+        super(texture, size, position);
+    }
+
+    public void makeDrunk(Player player){
+        player.isDrunk = true;
+        if (!player.canGetDrunk){
+        player.SwapControls();
+        }
+    }
+
+}
