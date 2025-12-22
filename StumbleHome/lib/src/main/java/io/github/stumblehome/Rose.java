ada30f370e7b654b5784f3612940d20514cb6909
Lenny Schwarz <lennyschwarz06@gmail.com>
2025-12-22 15:54:18 +0000

    Feat: CI updates

--- StumbleHome/lib/src/main/java/io/github/stumblehome/Rose.java
+++ StumbleHome/lib/src/main/java/io/github/stumblehome/Rose.java
@@ -2,11 +2,11 @@ package io.github.stumblehome;
 
 import com.badlogic.gdx.graphics.Texture;
 
-public class Rose extends CollectableEntity{
+public class Rose extends CollectableEntity {
 
     public boolean isSquished = false;
 
-    public Rose(Texture texture, float size, float[] position){
+    public Rose(Texture texture, float size, float[] position) {
         super(texture, size, position);
     }
 }
