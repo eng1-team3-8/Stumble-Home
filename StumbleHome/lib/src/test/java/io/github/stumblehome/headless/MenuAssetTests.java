3101a547090993c566d14a56888cb1801dfabee5
IsaacMile <isaacjmileham@gmail.com>
2026-01-02 16:58:09 +0000

    test: added asset tests for the menu assets

--- StumbleHome/lib/src/test/java/io/github/stumblehome/headless/MenuAssetTests.java
+++ StumbleHome/lib/src/test/java/io/github/stumblehome/headless/MenuAssetTests.java
@@ -9,10 +9,25 @@ import com.badlogic.gdx.Gdx;
 public class MenuAssetTests extends AbstractHeadlessGdxTest {
     
     @Test
-    public void testMainMenuAssets() {
+    public void testMenuAssets() {
         assertTrue(Gdx.files.internal("MainMenu.png").exists(), "Background image does not exist");
-        assertTrue(Gdx.files.internal("tutorial.png").exists(), "Tutorial image does not exist");
         assertTrue(Gdx.files.internal("ui/uiskin.json").exists(), "UI skin does not exist");
+    }
+
+    @Test
+    public void testMainMenuAssets() {
+        assertTrue(Gdx.files.internal("tutorial.png").exists(), "Tutorial image does not exist");
+    }
+
+    @Test
+    public void testLeaderboardAssets() {
+        assertTrue(Gdx.files.internal("leaderBoard.csv").exists(), "Leaderboard csv does not exist");
+    }
+
+    @Test
+    public void testGameFinishAssets() {
+        // Currently no custom assets for the end game screens. 
+        // this test exists for future development
     }
 
 }
