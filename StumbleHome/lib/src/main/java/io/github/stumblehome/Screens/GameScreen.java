7d7d96e896c32bb1b12413ffea3a712ecd0e881c
IsaacMile <isaacjmileham@gmail.com>
2026-01-06 18:43:23 +0000

    fix: formatting fix

--- StumbleHome/lib/src/main/java/io/github/stumblehome/Screens/GameScreen.java
+++ StumbleHome/lib/src/main/java/io/github/stumblehome/Screens/GameScreen.java
@@ -219,7 +219,11 @@ public class GameScreen implements Screen {
                 Color.RED,
                 2f);
         msg.addMessage(Messages.BOTHSQUISHED, "I see, you just harbour chaos", Color.BLACK, 2f);
-        msg.addMessage(Messages.CHAINSAWPICKEDUP, "You've obtained a chainsaw. press e to cut a line of hedges!", Color.RED, 2f);
+        msg.addMessage(
+                Messages.CHAINSAWPICKEDUP,
+                "You've obtained a chainsaw. press e to cut a line of hedges!",
+                Color.RED,
+                2f);
     }
 
     @Override
