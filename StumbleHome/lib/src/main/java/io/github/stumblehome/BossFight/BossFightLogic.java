2ca42752c2fd2705a9664e51fc7ee4b1fad628db
Lenny Schwarz <lennyschwarz06@gmail.com>
2026-01-07 13:55:25 +0000

    Feat: Added methods in boss fight logic to move the scissors and stop them if they are paused

--- StumbleHome/lib/src/main/java/io/github/stumblehome/BossFight/BossFightLogic.java
+++ StumbleHome/lib/src/main/java/io/github/stumblehome/BossFight/BossFightLogic.java
@@ -1,3 +1,26 @@
 package io.github.stumblehome.BossFight;
 
-public class BossFightLogic {}
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class BossFightLogic {
+
+    private boolean isStopped = false;
+
+    private void checkIfStopped() {
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            isStopped = true;
+        }
+    }
+
+    public void moveScissors(Viewport viewport, SpriteBatch batch, Scissors scissors) {
+        this.checkIfStopped();
+        if (!isStopped) {
+            scissors.move(viewport);
+        }
+
+        scissors.draw(batch);
+    }
+}
