16722a1b2ff702d2a7059d564009012c6b9165cb
IsaacMile <isaacjmileham@gmail.com>
2026-01-07 16:50:17 +0000

    fix: changed keycard and bottle so that they inherited from Colletable entity rather than Sprite

--- StumbleHome/lib/src/main/java/io/github/stumblehome/Entities/KeycardEvent.java
+++ StumbleHome/lib/src/main/java/io/github/stumblehome/Entities/KeycardEvent.java
@@ -1,12 +1,7 @@
 package io.github.stumblehome.Entities;
 
-import static java.lang.Math.pow;
-import static java.lang.Math.sqrt;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
@@ -14,52 +9,13 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
  * Represents a collectible keycard item required to unlock the exit. The keycard displays an
  * animated sprite until collected by the player.
  */
-public class KeycardEvent extends Sprite {
-
-    // Size of the keycard sprite when rendered.
-    public final float keycardSize = 1.0f;
-    // Spritesheet texture containing keycard animation frames.
-    public Texture keycardSheet;
-    // X-coordinate of the keycard on the map.
-    public float keycardX;
-    // Y-coordinate of the keycard on the map.
-    public float keycardY;
-    // Current animation being displayed.
-    private Animation<TextureRegion> currentAnimation;
-    // Previously displayed animation, used to detect animation changes.
-    private Animation<TextureRegion> previousAnimation;
-    // Elapsed time for the current animation.
-    private float stateTime;
-    // Whether the player has picked up the keycard.
-    private boolean collected = false;
+public class KeycardEvent extends CollectableEntity {
 
     /**
-     * Creates a new keycard event with the specified sprite.
-     *
-     * @param sprite the sprite containing the keycard texture
+     * calling constructor of superclass
      */
-    public KeycardEvent(Sprite sprite) {
-        super(sprite);
-        initializeAnimations();
-    }
-
-    /**
-     * Extracts animation frames from the keycard spritesheet. Sets up a 3-frame looping animation.
-     */
-    private void initializeAnimations() {
-        int frameWidth = 32;
-        int frameHeight = 32;
-
-        keycardSheet = this.getTexture();
-
-        // grab frames: top-left, middle-left, top-right
-        TextureRegion[] keycardFrames = new TextureRegion[3];
-        keycardFrames[0] = new TextureRegion(keycardSheet, 0, 0, frameWidth, frameHeight);
-        keycardFrames[1] = new TextureRegion(keycardSheet, 0, 32, frameWidth, frameHeight);
-        keycardFrames[2] = new TextureRegion(keycardSheet, 32, 0, frameWidth, frameHeight);
-
-        currentAnimation = new Animation<>(0.3f, keycardFrames);
-        stateTime = 0f;
+    public KeycardEvent(Texture texture, float size, float[] position) {
+        super(texture, size, position, 32, 32, 3);
     }
 
     /**
@@ -67,12 +23,12 @@ public class KeycardEvent extends Sprite {
      * hasn't been collected.
      */
     public void logic() {
-        if (!collected) {
-            if (currentAnimation != previousAnimation) {
-                stateTime = 0f;
-                previousAnimation = currentAnimation;
+        if (!isCollected) {
+            if (current_animation != previous_animation) {
+                state_time = 0f;
+                previous_animation = current_animation;
             }
-            stateTime += Gdx.graphics.getDeltaTime();
+            state_time += Gdx.graphics.getDeltaTime();
         }
     }
 
@@ -81,45 +37,11 @@ public class KeycardEvent extends Sprite {
      *
      * @param batch the sprite batch used for rendering
      */
-    public void draw(SpriteBatch batch) {
-        if (!collected) {
-            TextureRegion frameToDraw = currentAnimation.getKeyFrame(stateTime, true);
-            batch.draw(frameToDraw, keycardX, keycardY, keycardSize, keycardSize);
+    @Override
+    public void drawEntity(SpriteBatch batch) {
+        if (!isCollected) {
+            TextureRegion frameToDraw = current_animation.getKeyFrame(state_time, true);
+            batch.draw(frameToDraw, x_pos, y_pos, frame_size, frame_size);
         }
-    }
-
-    /**
-     * Checks whether the player has collided with the keycard using pythagoras theorem between the
-     * centres of the keycard and player.
-     *
-     * @param playerCentreX the player's centre x position
-     * @param playerCentreY the player's centre y position
-     * @return true if collision occurred, false otherwise
-     */
-    public boolean checkCollision(float playerCentreX, float playerCentreY) {
-        if (collected) {
-            return false;
-        }
-
-        boolean colliding;
-        // calculate distance between player centre and long boi centre using pythagoras
-        double distance =
-                sqrt(pow(playerCentreX - getCentreX(), 2) + pow(playerCentreY - getCentreY(), 2));
-
-        // set nearing to true if player within (value of radius) of long boi
-        colliding = distance < 1f;
-
-        if (colliding) {
-            collected = true;
-        }
-        return colliding;
-    }
-
-    private float getCentreX() {
-        return keycardX + keycardSize / 2;
-    }
-
-    private float getCentreY() {
-        return keycardY + keycardSize / 2;
     }
 }
