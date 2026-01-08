a6fa87e64a8d3c0aa9393d2905336a410e9339f0
Lenny Schwarz <lennyschwarz06@gmail.com>
2026-01-08 22:23:24 +0000

    Feat: Scaled for increase or decrease in viewport

--- StumbleHome/lib/src/main/java/io/github/stumblehome/Entities/BossFightEntity.java
+++ StumbleHome/lib/src/main/java/io/github/stumblehome/Entities/BossFightEntity.java
@@ -3,6 +3,7 @@ package io.github.stumblehome.Entities;
 import static java.lang.Math.*;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /**
  * This is an entity class for sprites that are in the boss fight
@@ -24,16 +25,20 @@ public class BossFightEntity extends NonPlayerEntity {
     }
 
     @Override
-    public void logic() {}
+    public boolean checkColliding(float playerCentreX, float playerCentreY) {
+        return false;
+    }
 
     @Override
-    public boolean checkColliding(float playerCentreX, float playerCentreY) {
+    public void logic() {}
+
+    public boolean checkOverlap(float playerCentreX, FitViewport viewport) {
 
         double distance = abs(playerCentreX - this.getX());
 
         System.out.println(distance + " " + playerCentreX + " " + this.getX());
 
-        return distance < 30f;
+        return distance < (viewport.getWorldWidth() / 80) * 3;
     }
 
     public void updatePosition() {
