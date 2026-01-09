a21be1b444fe929c6d5506e50fca819e646f4314
zayKenyon <me@knyn.uk>
2026-01-09 18:59:30 +0000

    fix: losslessly optimise new assets and correct path

--- StumbleHome/lib/src/main/java/io/github/stumblehome/Entities/Bob.java
+++ StumbleHome/lib/src/main/java/io/github/stumblehome/Entities/Bob.java
@@ -3,7 +3,7 @@ package io.github.stumblehome.Entities;
 import com.badlogic.gdx.graphics.Texture;
 
 public class Bob extends CollectableEntity {
-    public static final String ASSET = "Sprites/B-bthing.png";
+    public static final String ASSET = "Sprites/B-bThing.png";
 
     public Bob(Texture texture, float size, float[] position) {
         super(texture, size, position);
