aa08c5c4195c892c0af5f1936a62c104a48bc28f
Lenny Schwarz <lennyschwarz06@gmail.com>
2026-01-09 16:26:11 +0000

    Feat: Added a bird seed entity

---
+++ StumbleHome/lib/src/main/java/io/github/stumblehome/Entities/BirdSeed.java
@@ -0,0 +1,15 @@
+package io.github.stumblehome.Entities;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class BirdSeed extends CollectableEntity{
+
+  public BirdSeed(Texture texture, float size, float[] position) {
+    super(texture, size, position);
+  }
+
+  @Override
+  public void logic() {
+
+  }
+}
