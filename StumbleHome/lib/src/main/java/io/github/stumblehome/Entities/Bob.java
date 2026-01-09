d34dcbddbcf21dda2dc907509361def6fb133800
zayKenyon <me@knyn.uk>
2026-01-09 19:37:30 +0000

    fix: bob asset path; mirrors a21be1

--- StumbleHome/lib/src/main/java/io/github/stumblehome/Entities/Bob.java
+++ StumbleHome/lib/src/main/java/io/github/stumblehome/Entities/Bob.java
@@ -3,11 +3,11 @@ package io.github.stumblehome.Entities;
 import com.badlogic.gdx.graphics.Texture;
 
 public class Bob extends CollectableEntity {
-    public static final String ASSET = "Sprites/B-bthing.png";
+  public static final String ASSET = "Sprites/B-bThing.png";
 
-    public Bob(Texture texture, float size, float[] position) {
-        super(texture, size, position);
-    }
+  public Bob(Texture texture, float size, float[] position) {
+    super(texture, size, position);
+  }
 
-    public void logic() {}
+  public void logic() {}
 }
