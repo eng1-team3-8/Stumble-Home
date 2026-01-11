87cc0c3981599e92d8ea1204a4f29cdfc9db3522
Starman7312 <henrygillett@duck.com>
2026-01-11 20:41:59 +0000

    fix: unused test variables removed

--- StumbleHome/lib/src/test/java/io/github/stumblehome/headless/EventTests/ChainsawRemoveWallTests.java
+++ StumbleHome/lib/src/test/java/io/github/stumblehome/headless/EventTests/ChainsawRemoveWallTests.java
@@ -17,7 +17,6 @@ public class ChainsawRemoveWallTests extends AbstractHeadlessGdxTest {
     private Chainsaw chainsaw;
     private Player testPlayer;
     private TiledMapTileLayer testCollisionLayer;
-    private TiledMapTileLayer initalCollisionLayer;
     private EntityDirection direction;
     private int[] pos;
 
@@ -26,8 +25,6 @@ public class ChainsawRemoveWallTests extends AbstractHeadlessGdxTest {
         // Load Map and get collision layers
         TiledMap testMap = new TmxMapLoader().load("map2.tmx");
         testCollisionLayer = (TiledMapTileLayer) testMap.getLayers().get("hedge");
-        TiledMap testMap2 = new TmxMapLoader().load("map2.tmx");
-        initalCollisionLayer = (TiledMapTileLayer) testMap2.getLayers().get("hedge");
 
         // Set up map size to be the same as the game
         int mapWidthInTiles = testMap.getProperties().get("width", Integer.class);
